d2ff6b1d6077854cc656d48c3949540ccee0118e
Macil Tech <maciltech@gmail.com>
2012-07-05 09:41:56 -0600

    Version 1.9.

--- QuickBlocker.user.js
+++ QuickBlocker.user.js
@@ -6,7 +6,7 @@
 // @include      http*://boards.4chan.org/b/res/*
 // @updateURL    https://raw.github.com/Macil/QuickBlocker/master/QuickBlocker.user.js
 // @homepage     http://macil.github.com/QuickBlocker/
-// @version      1.8
+// @version      1.9
 // @icon         http://i.imgur.com/aUTYg.png
 // ==/UserScript==
 
