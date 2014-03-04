01e4b8efaa468167c815b8338533d398ad152513
specialeddy <specialeddy@users.noreply.github.com>
2014-03-04 07:52:31 -0800

    Update QuickBlocker.user.js
    Forgot to make it reflect my fork

--- QuickBlocker.user.js
+++ QuickBlocker.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Quick Blocker
-// @namespace    macil
+// @namespace    eddy
 // @description  Quickly block a single poster on /b/
-// @author       Macil
+// @author       Eddy
 // @include      http*://boards.4chan.org/b/res/*
-// @updateURL    https://raw.github.com/Macil/QuickBlocker/master/QuickBlocker.user.js
-// @homepage     http://macil.github.com/QuickBlocker/
-// @version      1.11
+// @updateURL    https://raw.github.com/specialeddy/QuickBlocker/master/QuickBlocker.user.js
+// @homepage     http://specialeddy.github.com/QuickBlocker/
+// @version      1.12
 // @icon         http://i.imgur.com/aUTYg.png
 // ==/UserScript==
 
