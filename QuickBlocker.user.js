3022a0abb5f96b41dda98ffda4923464e07eecf7
specialeddy <eddyis_special5.s@outlook.com>
2014-10-26 08:42:19 +0200

    1.19 Universal script support URL slug fix

--- QuickBlocker.user.js
+++ QuickBlocker.user.js
@@ -6,7 +6,7 @@
 // @author       Eddy
 // @include      http*://boards.4chan.org/b/thread/*
 // @updateURL    https://raw.github.com/specialeddy/QuickBlocker/master/QuickBlocker.user.js
-// @version      1.18
+// @version      1.19
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB8AAAAfCAYAAAAfrhY5AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAANNJREFUeNrkl0sOwyAQQ8H36ul7MKJEEa0iPmPPFBZlgbJAfgYmBnIpJd2tfixo+epu+EpwNXDCK/id88+Jrw8uVfgK8NMA0saGkbvvJVJnONLAbF9UAxYNsEKR45vwVvFZBVvjesXcnbligAFPl50xwIJNe24xoIDNBTcyoILphLMUHaNDJdxMmI1oOl57AOVsgCe5PEFEw5X/PARurWrGALxgjwFEzFg1gKisVgwg6pBQDCAKrBiAJ1i846eXCfVWa9H436vz9hfL1rfatlfqIcAAcBeePao2b20AAAAASUVORK5CYII=
 // ==/UserScript==
 
@@ -553,7 +553,7 @@ function qbmain() {
     }
 
     function getThreadNum() {
-        return /\/(\d+)(#.*)?$/.exec(document.URL)[1];
+        return /\/(\d+)(.*)?$/.exec(document.URL)[1];
     }
 
     storageKey = storageKeyHeader+getThreadNum();
