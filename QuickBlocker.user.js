875b9696699c1f9c691a8a94c839080bdbec77cb
specialeddy <eddyis_special5.s@outlook.com>
2014-10-27 04:15:00 +0200

    1.20 - Restored blocked quote crossout functionality

--- QuickBlocker.user.js
+++ QuickBlocker.user.js
@@ -6,7 +6,7 @@
 // @author       Eddy
 // @include      http*://boards.4chan.org/b/thread/*
 // @updateURL    https://raw.github.com/specialeddy/QuickBlocker/master/QuickBlocker.user.js
-// @version      1.19
+// @version      1.20
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB8AAAAfCAYAAAAfrhY5AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAANNJREFUeNrkl0sOwyAQQ8H36ul7MKJEEa0iPmPPFBZlgbJAfgYmBnIpJd2tfixo+epu+EpwNXDCK/id88+Jrw8uVfgK8NMA0saGkbvvJVJnONLAbF9UAxYNsEKR45vwVvFZBVvjesXcnbligAFPl50xwIJNe24xoIDNBTcyoILphLMUHaNDJdxMmI1oOl57AOVsgCe5PEFEw5X/PARurWrGALxgjwFEzFg1gKisVgwg6pBQDCAKrBiAJ1i846eXCfVWa9H436vz9hfL1rfatlfqIcAAcBeePao2b20AAAAASUVORK5CYII=
 // ==/UserScript==
 
@@ -317,7 +317,7 @@ function qbmain() {
         blockPostContent(post);
         blocked_post_count++;
 
-        var postnumber = parseInt( $(".postInfo .postNum a[title='Quote this post']", post).first().text() );
+        var postnumber = parseInt( $("input[value='delete']", post).attr("name") );
         blocked_post_numbers[postnumber] = true;
     }
 
