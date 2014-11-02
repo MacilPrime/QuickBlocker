d9c407e92c95fedb209e5f9890c9e9cf07d97b9a
specialeddy <eddyis_special5.s@outlook.com>
2014-11-02 03:11:28 +0200

    Removed superfluous function call (developer jargon)

--- QuickBlocker.user.js
+++ QuickBlocker.user.js
@@ -6,7 +6,7 @@
 // @author       Eddy
 // @include      http*://boards.4chan.org/b/thread/*
 // @updateURL    https://raw.github.com/specialeddy/QuickBlocker/master/QuickBlocker.user.js
-// @version      1.21
+// @version      1.22
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB8AAAAfCAYAAAAfrhY5AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAANNJREFUeNrkl0sOwyAQQ8H36ul7MKJEEa0iPmPPFBZlgbJAfgYmBnIpJd2tfixo+epu+EpwNXDCK/id88+Jrw8uVfgK8NMA0saGkbvvJVJnONLAbF9UAxYNsEKR45vwVvFZBVvjesXcnbligAFPl50xwIJNe24xoIDNBTcyoILphLMUHaNDJdxMmI1oOl57AOVsgCe5PEFEw5X/PARurWrGALxgjwFEzFg1gKisVgwg6pBQDCAKrBiAJ1i846eXCfVWa9H436vz9hfL1rfatlfqIcAAcBeePao2b20AAAAASUVORK5CYII=
 // ==/UserScript==
 
@@ -518,15 +518,14 @@ function qbmain() {
             if(tag.hasClass("replyContainer")) {
                 if(!tag.parent().is(".inline, #qp")) {
                     addButton(tag);
-                    updateBlockedCount();
                     var posteruid = $(".posteruid", tag).first().text();
 
                     if(blockedIDs[posteruid]) {
                         removePostHidden(tag);
-                        updateBlockedCount();
                     } else if(checkPostContent(tag)) {
                         blockID(posteruid);
                     }
+                    updateBlockedCount();
                 } else {
                     // Always show inlined posts
                     if(tag.is(":hidden")) {
